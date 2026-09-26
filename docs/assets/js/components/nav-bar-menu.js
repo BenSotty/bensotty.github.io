5938c928df5e0bfd7d5d9394f891e6cc3b92a1bc
Ben Sotty <benjamin.sotty@gmail.com>
2026-09-26 14:04:30 +0200

    Automatically xlose menu after click on link

---
+++ docs/assets/js/components/nav-bar-menu.js
@@ -0,0 +1,28 @@
+import { Offcanvas } from 'bootstrap';
+
+export const NAV_BAR_MENU_ID = 'offcanvasNavbar';
+
+export class NavBarMenu {
+  constructor(id) {
+    this.delayMs = 700;
+    this.menuElt = document.getElementById(id);
+    this.menu = Offcanvas.getOrCreateInstance(this.menuElt);
+    this.links = [...this.menuElt.querySelectorAll('.nav-link')];
+    this.links.forEach((link) => {
+      link.addEventListener('click', e => {
+        this.onLinkClicked();
+      });
+    });
+  }
+  onLinkClicked () {
+    if (this.isMenuOpen()) {
+      setTimeout(this.closeMenu.bind(this), this.delayMs);
+    }
+  }
+  isMenuOpen () {
+    return this.menuElt.classList.contains('show');
+  }
+  closeMenu () {
+    this.menu.hide();
+  }
+}
--- docs/assets/js/on-dom-ready.js
+++ docs/assets/js/on-dom-ready.js
@@ -1,11 +1,13 @@
 import { initPageBookingBtns } from './components/booking-btns';
 import { initPageCollapseableLists } from './components/collapsable-list';
 import {initPageExpandableTextLists } from './components/expandable-text-list';
+import { NavBarMenu, NAV_BAR_MENU_ID } from  './components/nav-bar-menu';
 
 export default class OnDomReady {
   run () {
     document.querySelector("body").classList.remove("js-not-loaded");
     initPageBookingBtns();
+    new NavBarMenu(NAV_BAR_MENU_ID);
     initPageCollapseableLists();
     initPageExpandableTextLists();
   }
